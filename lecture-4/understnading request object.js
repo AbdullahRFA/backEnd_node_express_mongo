7228bda525e965c325df09fdbb4fd840108ea5c9
AbdullahRFA <shakibrybmn@gmail.com>
2026-02-19 23:08:20 +0600

    understand request object

--- lecture-4/understnading request object.js
+++ lecture-4/understnading request object.js
@@ -1,16 +1,16 @@
 const http = require("http");
 
 const server = http.createServer((req, res) => {
+  console.log(req.method);
   console.log(req.url);
+  console.log(req.headers);
+  res.write("Hello from Node server");
+  // res.end();
+  // process.exit();
 
-  res.end("Server will close now");
-
-  server.close(() => {
-    console.log("Server closed");
-  });
 });
 
-const port = 3001;
+const port = 3000;
 
 server.listen(port, () => {
   console.log(`Server is listening at http://localhost:${port}`);
