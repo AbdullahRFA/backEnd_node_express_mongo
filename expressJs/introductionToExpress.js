fabdf30845408dd41b8f623fbfab4b9fe1cd0ed3
AbdullahRFA <shakibrybmn@gmail.com>
2026-02-22 22:16:10 +0600

    send response and remove http core module cause now express create server and do the rest of the things

--- expressJs/introductionToExpress.js
+++ expressJs/introductionToExpress.js
@@ -1,5 +1,5 @@
 // core modules
-const http = require("http");
+// const http = require("http");
 // local modules
 const requestHandler = require("./requestHandler");
 //external modules
@@ -12,18 +12,23 @@ app.use(
         console.log("This is a first middleware function");
         console.log(req.method);
         next();
+        // res.send("<h1>Congratulations from Express server</h1>")
     }
 )
 app.use(
     (req,res,next)=>{
         console.log("This is a second middleware function");
         console.log(req.url);
+        res.send("<h1>Hello from Express server</h1>");
         // next();
     }
 )
-const server = http.createServer(app);
+// const server = http.createServer(app);
 const port = 3000;
 
-server.listen(port, () => {
+// server.listen(port, () => {
+//   console.log(`Server is listening at http://localhost:${port}`);
+// });
+app.listen(port, () => {
   console.log(`Server is listening at http://localhost:${port}`);
 });
